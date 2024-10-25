dcf75677bf2376d301869e4922a8bdb4311e8c80
alexjr17 <alexjose.r.r@gmail.com>
2024-10-24 22:22:09 -0500

    quitar tabla

--- database/migrations/2024_10_24_192950_create_configs_table.php
+++ database/migrations/2024_10_24_192950_create_configs_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('configuraciones');
+        // Schema::dropIfExists('configuraciones');
     }
 
 };
